ae0dad4763c00103d837e45f6183a5f580fbd5f4
Nishchaymodi8 <nishchaymodi8@gmail.com>
2026-03-28 15:42:59 +0530

    CHECKING 3

--- canvascart/settings.py
+++ canvascart/settings.py
@@ -30,14 +30,15 @@ ALLOWED_HOSTS = ['.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
+          'cloudinary',
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-      'cloudinary',
-    'cloudinary_storage',
+
     'home',
     'gallery',
     'cart',
